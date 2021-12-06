1a4b0c1fec65f770306ef361011e9c476e9aa0e4
kapsteja <teja@kapstonellc.com>
2021-12-06 18:15:47 +0530

    new commit on test

--- _test_/index.test.js
+++ _test_/index.test.js
@@ -2,7 +2,12 @@ const request = require("supertest");
 const app = require("../index");
 
 describe("Get route", () => {
-  
+  it("page should return hello world", async () => {
+    const res = await request(app).get("/");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual("Hello world");
+    done();
+  });
 });
 
 afterAll(async () => {
