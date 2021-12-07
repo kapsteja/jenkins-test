fa64aadcd5d6a35332115f1a7733f73533a7e7a7
kapsteja <teja@kapstonellc.com>
2021-12-07 15:06:59 +0530

    teja commited

--- _test_/index.test.js
+++ _test_/index.test.js
@@ -2,10 +2,10 @@ const request = require("supertest");
 const app = require("../index");
 
 describe("Get route", () => {
-  it("page should return hello teja", async () => {
+  it("page should return hello Deva", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toEqual(200);
-    expect(res.body).toEqual("Hello teja");
+    expect(res.body).toEqual("Hello Deva");
    
   });
 });
